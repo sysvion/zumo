0486ee5fff9ab642ea1cebae54965c10f3f321b4
Aron <aron@dingweb.nl>
2023-05-16 16:44:08 +0200

    test

--- zumo_xbee.ino
+++ zumo_xbee.ino
@@ -26,7 +26,7 @@ void setup()
 {
   Serial.begin(9600);         //start serial connection with the Arduino serial
   Serial1.begin(9600);        //start serial connection with the XCTU application (Xbee serial)
-
+   // test;
   volume += 6;                //6 is added to convert value to min/max of the sound. The volume range that the user sees ranges from 0 to 7 but in reality this ranges from 6 t 13
 
   while (!Serial) {}          //waits for serial connection to establish
